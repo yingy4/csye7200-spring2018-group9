a2ea42fc162eb4e2361fbf4b3a601be72271d4a5
001239511ShuangShuangXu <xu.shua@husky.neu.edu>
2018-03-26 22:50:32 -0400

    program

---
+++ target/scala-2.12/routes/main/controllers/ReverseRoutes.scala
@@ -0,0 +1,74 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:D:/Study/BigData/Assignment/Project/csye7200-spring2018-group9/conf/routes
+// @DATE:Thu Mar 22 12:12:25 EDT 2018
+
+import play.api.mvc.Call
+
+
+import _root_.controllers.Assets.Asset
+
+// @LINE:7
+package controllers {
+
+  // @LINE:11
+  class ReverseAsyncController(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:11
+    def message(): Call = {
+      
+      Call("GET", _prefix + { _defaultPrefix } + "message")
+    }
+  
+  }
+
+  // @LINE:7
+  class ReverseHomeController(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:7
+    def index(): Call = {
+      
+      Call("GET", _prefix)
+    }
+  
+  }
+
+  // @LINE:14
+  class ReverseAssets(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:14
+    def versioned(file:Asset): Call = {
+      implicit lazy val _rrc = new play.core.routing.ReverseRouteContext(Map(("path", "/public"))); _rrc
+      Call("GET", _prefix + { _defaultPrefix } + "assets/" + implicitly[play.api.mvc.PathBindable[Asset]].unbind("file", file))
+    }
+  
+  }
+
+  // @LINE:9
+  class ReverseCountController(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def count(): Call = {
+      
+      Call("GET", _prefix + { _defaultPrefix } + "count")
+    }
+  
+  }
+
+
+}
